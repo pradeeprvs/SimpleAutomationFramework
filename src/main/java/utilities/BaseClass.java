75b67499cc91f2e2b6804518605cc1ab518fe836
Pradeep R V S <pradeeptheleader@gmail.com>
2019-05-21 18:04:14 +0530

    Committed on 21.05.2019 EOD

---
+++ src/main/java/utilities/BaseClass.java
@@ -0,0 +1,106 @@
+package utilities;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+
+public class BaseClass {
+
+	protected WebDriver driver;
+	Properties prop =new Properties();
+	public ExtentHtmlReporter htmlReporter; //responsible for the look and feel of the extent reports
+	public ExtentReports extent; //Responsible for adding System related information
+	public ExtentTest test; //Responsible for logging and adding screenshots to the reports
+
+
+	public void createReport() throws UnknownHostException {
+		htmlReporter =new ExtentHtmlReporter(System.getProperty("user.dir")+"/test-output/my-report.html"); //you need to specify the path where you wanted to store your reports
+		htmlReporter.config().setTheme(Theme.DARK);
+		htmlReporter.config().setDocumentTitle("My Extent Reports");
+		htmlReporter.config().setReportName("Functional Testing Reports");
+
+		extent=new ExtentReports();
+		extent.attachReporter(htmlReporter);
+
+		//Passing General/System related information
+		InetAddress addr = InetAddress.getLocalHost();//this is to get the system related information using java methods
+		extent.setSystemInfo("HostName", addr.getHostName());
+		extent.setSystemInfo("OS", "Windows10");
+		extent.setSystemInfo("Environment", "QA");
+		extent.setSystemInfo("IP", addr.getHostAddress());
+		System.out.println("I am from Create Report");
+	}
+
+	public WebDriver initialise_WebDriver() throws IOException {
+		FileInputStream fis= new FileInputStream("C:\\Users\\pravinutala\\SimpleFramework\\src\\main\\java\\resources\\config.properties");
+		prop.load(fis);
+		if(prop.getProperty("browser").equalsIgnoreCase("Chrome")) {
+			this.driver=new ChromeDriver();
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\pravinutala\\Downloads\\Jars\\Selenium Jars\\chromedriver_win32\\chromedriver.exe");
+			driver.manage().deleteAllCookies();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}
+
+		if(prop.getProperty("browser").equalsIgnoreCase("firefox")) {
+			//firefox implementation code must be here
+		}
+		return driver;
+	}
+
+	public void endReport() {
+		extent.flush();
+	}
+
+	public void tearDown(ITestResult result) throws IOException {
+		System.out.println("I am from TearDown");
+		if(result.getStatus()==ITestResult.SUCCESS) { //when test case got passed, we are logging this into report
+			//			String screenshotPath = getScreenshot(driver, result.getName());
+			//			   test.addScreenCaptureFromPath(screenshotPath); //adding screenshot
+			test.log(Status.PASS,result.getName() + " test case is passed");
+		}
+
+		if(result.getStatus()==ITestResult.FAILURE) {
+			test.log(Status.FAIL, "TEST CASE FAILED IS " + result.getName()); // to add name in extent report
+			test.log(Status.FAIL, "TEST CASE FAILED IS " + result.getThrowable()); // to add error/exception in extent report
+			String screenshotPath = getScreenshot(driver, result.getName());
+			test.addScreenCaptureFromPath(screenshotPath);// adding screen shot
+		} else if (result.getStatus() == ITestResult.SKIP) {
+			test.log(Status.SKIP, "Test Case SKIPPED IS " + result.getName());
+		}
+		driver.quit();
+	}
+
+	public String getScreenshot(WebDriver driver, String Screenshotname) throws IOException {
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+
+		// after execution, you could see a folder "FailedTestsScreenshots" under src folder
+		String destination = System.getProperty("user.dir") + "/Screenshots/" + Screenshotname + dateName + ".png";
+		File finalDestination = new File(destination);
+		FileUtils.copyFile(source, finalDestination);
+		return destination;
+
+	}
+}
---
+++ src/test/java/AutomationFramework_Selenium/OrangeHRM_Test.java
@@ -0,0 +1,73 @@
+package AutomationFramework_Selenium;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import utilities.BaseClass;
+
+public class OrangeHRM_Test extends BaseClass{
+
+	@BeforeMethod
+	public void invoke_WebDriver() throws IOException {
+		driver=initialise_WebDriver();
+	}
+	
+	@BeforeTest
+	public void CreateReport() throws UnknownHostException {
+		createReport();
+	}
+
+	@Test
+	public void openURL() {
+		test= extent.createTest("Open URL Test");
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+	}
+
+	@Test
+	public void loginTest() {
+		test= extent.createTest("Login Test");
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		//login
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+	}
+	
+	@Test
+	public void verifyLogo() {
+		test= extent.createTest("Verify Logo Test");
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Assert.assertTrue(driver.findElement(By.xpath(".//div[@id='divLogo']/img")).isDisplayed());
+	}
+	
+	@Test
+	public void createNodeTest(  ) {
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		test = extent.createTest("OrangeHRMcreateNodeTest");
+		test.createNode("Login with Valid input");
+		Assert.assertTrue(true);
+
+		test.createNode("Login with In-valid input");
+		Assert.assertTrue(true);
+	}
+
+	
+	@AfterTest
+	public void endReports() {
+		endReport();
+	}
+
+	@AfterMethod
+	public void tearDown_TestObjects(ITestResult result) throws IOException {
+		tearDown(result);
+	}
+}
