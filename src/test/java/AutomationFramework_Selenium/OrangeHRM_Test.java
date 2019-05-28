98d85a5d42837703be06442d05736ae58f4e9dd0
Pradeep R V S <pradeeptheleader@gmail.com>
2019-05-28 12:56:29 +0530

    Commit

--- src/test/java/AutomationFramework_Selenium/OrangeHRM_Test.java
+++ src/test/java/AutomationFramework_Selenium/OrangeHRM_Test.java
@@ -1,5 +1,7 @@
 package AutomationFramework_Selenium;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import utilities.BaseClass;
@@ -12,31 +14,31 @@ public class OrangeHRM_Test extends BaseClass{
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 	}
 
-//	@Test
-//	public void loginTest() {
-//		test= extent.createTest("Login Test");
-//		driver.get("https://opensource-demo.orangehrmlive.com/");
-//		//login
-//		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
-//		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
-//		driver.findElement(By.id("btnLogin")).click();
-//	}
-//	
-//	@Test
-//	public void verifyLogo() {
-//		test= extent.createTest("Verify Logo Test");
-//		driver.get("https://opensource-demo.orangehrmlive.com/");
-//		Assert.assertTrue(driver.findElement(By.xpath(".//div[@id='divLogo']/img")).isDisplayed());
-//	}
-//	
-//	@Test
-//	public void createNodeTest(  ) {
-//		driver.get("https://opensource-demo.orangehrmlive.com/");
-//		test = extent.createTest("OrangeHRMcreateNodeTest");
-//		test.createNode("Login with Valid input");
-//		Assert.assertTrue(true);
-//
-//		test.createNode("Login with In-valid input");
-//		Assert.assertTrue(true);
-//	}
+	@Test
+	public void loginTest() {
+		test= extent.createTest("Login Test");
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		//login
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+	}
+	
+	@Test
+	public void verifyLogo() {
+		test= extent.createTest("Verify Logo Test");
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Assert.assertTrue(driver.findElement(By.xpath(".//div[@id='divLogo']/img")).isDisplayed());
+	}
+	
+	@Test
+	public void createNodeTest(  ) {
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		test = extent.createTest("OrangeHRMcreateNodeTest");
+		test.createNode("Login with Valid input");
+		Assert.assertTrue(true);
+
+		test.createNode("Login with In-valid input");
+		Assert.assertTrue(true);
+	}
 }
