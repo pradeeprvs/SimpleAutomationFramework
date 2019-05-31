2241b32925e9958d28638f5840000c9e3cc1c162
Pradeep R V S <pradeeptheleader@gmail.com>
2019-05-31 12:59:35 +0530

    perfect with Extent Reports

--- src/main/java/utilities/BaseClass.java
+++ src/main/java/utilities/BaseClass.java
@@ -38,8 +38,8 @@ public class BaseClass {
 
 	@BeforeTest()
 	public void createReport() throws UnknownHostException {
-		htmlReporter =new ExtentHtmlReporter("test-output/extentReports/my-report "+ dateName +".html"); //you need to specify the path where you wanted to store your reports
-		htmlReporter =new ExtentHtmlReporter("test-output/extentReports/my-report.html");
+		//htmlReporter =new ExtentHtmlReporter("test-output/extentReports/my-report "+ dateName +".html");//use this when you want save your reports for the tracking purposes
+		htmlReporter =new ExtentHtmlReporter("test-output/extentReports/my-report.html"); //you need to specify the path where you wanted to store your reports, whenever you run the script,the new report coming will override this report
 		htmlReporter.config().setTheme(Theme.DARK);
 		htmlReporter.config().setDocumentTitle("My Extent Reports");
 		htmlReporter.config().setReportName("Functional Testing Reports");
