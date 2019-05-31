dbc6596c53b14c36fe231d3ea8e15b5071faf758
Pradeep R V S <pradeeptheleader@gmail.com>
2019-05-31 14:21:48 +0530

    All Set

--- src/main/java/utilities/BaseClass.java
+++ src/main/java/utilities/BaseClass.java
@@ -15,6 +15,8 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
@@ -34,12 +36,12 @@ public class BaseClass {
 	public ExtentHtmlReporter htmlReporter; //responsible for the configuration of look and feel of the extent reports
 	public ExtentReports extent; //Responsible for adding System related information
 	public ExtentTest test; //Responsible for logging and adding screenshots to the reports
-	String dateName = new SimpleDateFormat(" yyyy MM dd hh mm ss").format(new Date());//Just to add time to the filenames to make them dynamic
+	String dateName = new SimpleDateFormat(" yyyy_MM_dd_hh_mm_ss").format(new Date());//Just to add time to the filenames to make them dynamic
 
 	@BeforeTest()
 	public void createReport() throws UnknownHostException {
 		//htmlReporter =new ExtentHtmlReporter("test-output/extentReports/my-report "+ dateName +".html");//use this when you want save your reports for the tracking purposes
-		htmlReporter =new ExtentHtmlReporter("test-output/extentReports/my-report.html"); //you need to specify the path where you wanted to store your reports, whenever you run the script,the new report coming will override this report
+		htmlReporter =new ExtentHtmlReporter("test-output/extentReports/my-report.html"); //you need to specify the path where you wanted to store your reports, whenever you the new report coming will override this report
 		htmlReporter.config().setTheme(Theme.DARK);
 		htmlReporter.config().setDocumentTitle("My Extent Reports");
 		htmlReporter.config().setReportName("Functional Testing Reports");
@@ -61,6 +63,10 @@ public class BaseClass {
 		FileInputStream fis= new FileInputStream("src\\main\\java\\resources\\config.properties");
 		prop.load(fis);
 		if(prop.getProperty("browser").equalsIgnoreCase("Chrome")) {
+//			DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+//			ChromeOptions options = new ChromeOptions();
+//			options.addArguments("--headless");//This is needed to run headless browser tests
+//			capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 			this.driver=new ChromeDriver();
 			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 			driver.manage().deleteAllCookies();
@@ -83,8 +89,8 @@ public class BaseClass {
 	public void tearDown(ITestResult result) throws IOException {
 		System.out.println("I am from TearDown");
 		if(result.getStatus()==ITestResult.SUCCESS) { //when test case got passed, we are logging this into report
-			//			String screenshotPath = getScreenshot(driver, result.getName());
-			//			   test.addScreenCaptureFromPath(screenshotPath); //adding screenshot
+						String screenshotPath = getScreenshot(driver, result.getName());
+						   test.addScreenCaptureFromPath(screenshotPath); //adding screenshot
 			test.log(Status.PASS,result.getName() + " test case is passed");
 		}
 
@@ -104,7 +110,7 @@ public class BaseClass {
 		File source = ts.getScreenshotAs(OutputType.FILE);
 
 		// after execution, you could see a folder "FailedTestsScreenshots" under src folder
-		String destination = "/Screenshots/" + Screenshotname + dateName + ".png";
+		String destination = System.getProperty("user.dir") + "/Screenshots/" + Screenshotname + dateName + ".png";
 		File finalDestination = new File(destination);
 		FileUtils.copyFile(source, finalDestination);
 		return destination;
